c5ef9a5bcf9358c784c9006216189a7d06a8adbe
J M Adis <jessicaadis7@gmail.com>
2023-11-27 19:24:42 +0000

    have defined HTML selectors & validated elements

--- calculator-proj2/src/main.ts
+++ calculator-proj2/src/main.ts
@@ -2,4 +2,24 @@ import '../SCSS/main.css';
 
 console.log("Hello World!")
 
- 
+// HTML SELECTOR
+const displayInput = document.querySelector<HTMLElement>(".calculator__output--input")
+const displayResult = document.querySelector<HTMLElement>(".calculator__output--result") 
+const calcButtons = document.querySelectorAll<HTMLButtonElement>(".calculator__buttons--indiv")
+
+//ELEMENT VALIDATION
+if(!displayInput || !displayResult){
+    throw new Error("Issue with the selector of our container")
+}
+
+if(calcButtons.length ===0){
+    throw new Error("Issue with the QuerySelectorAll");
+
+}
+
+// HANDLER FUNCTIONS 
+const handleButtonClick = (event: Event) =>{
+    
+}
+
+//
